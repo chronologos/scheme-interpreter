988216d03ff2176b69fb1aeca6ce3579777cc2c9
Ian Tay <skrpfn@lavabit.com>
2017-11-28 09:16:10 -0500

    readme and improve test coverage

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# write-yourself-scheme
+# Scheme Interpreter
+My personal project for learning Haskell and Scheme. Based on [Write Yourself A Scheme](https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours). Comes with a test suite!
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,12 +6,21 @@ import           Test.QuickCheck
 main :: IO ()
 main = hspec $ do
   describe "test eval 1" $ do
-    it "evals atom" $ do
+    it "evals Atom" $ do
       res <- Lib.test ["'atom"]
       res `shouldBe` "atom"
-    it "evals string" $ do
+    it "evals String" $ do
       res <- Lib.test ["\"a string\""]
       res `shouldBe` "\"a string\""
+    it "evals Bool (atom)" $ do
+      res <- Lib.test ["#t"]
+      res `shouldBe` "#t"
+    it "evals DottedList (quoted)" $ do
+      res <- Lib.test ["(quote ((1 2) . 1))"]
+      res `shouldBe` "((1 2) . 1)"
+    it "evals DottedList (sugared)" $ do
+      res <- Lib.test ["'((1 2) . 1)"]
+      res `shouldBe` "((1 2) . 1)"
     it "evals simple numericBinop" $ do
       res <- Lib.test["(+ 2 2)"]
       res  `shouldBe` "4"
@@ -37,6 +46,24 @@ main = hspec $ do
       res `shouldBe` "#f"
     it "evals strBoolBinop" $ do
       res <- Lib.test ["(string=? \"test\"  \"test\")"]
+      res `shouldBe` "#t"
+    it "evals string? 1" $ do
+      res <- Lib.test ["(string? \"test\")"]
+      res `shouldBe` "#t"
+    it "evals string? 2" $ do
+      res <- Lib.test ["(string? 1)"]
+      res `shouldBe` "#f"
+    it "evals number? 1" $ do
+      res <- Lib.test ["(number? \"test\")"]
+      res `shouldBe` "#f"
+    it "evals number? 2" $ do
+      res <- Lib.test ["(number? 1)"]
+      res `shouldBe` "#t"
+    it "evals symbol? 1" $ do
+      res <- Lib.test ["(symbol? \"test\")"]
+      res `shouldBe` "#f"
+    it "evals symbol? 2" $ do
+      res <- Lib.test ["(symbol? 'atom)"]
       res `shouldBe` "#t"
     it "handles conditionals" $ do
       res <- Lib.test ["(if (> 2 3) \"no\" \"yes\")"]
